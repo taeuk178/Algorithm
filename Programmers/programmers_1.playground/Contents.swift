525320a7f6751e9d5be3e9f91bd108796a93329d
taeuk <nous178@naver.com>
2020-10-25 22:49:04 +0900

    commit

--- Programmers/programmers_1.playground/Contents.swift
+++ Programmers/programmers_1.playground/Contents.swift
@@ -2,6 +2,7 @@ import UIKit
 
 func solution(_ numbers: [Int]) -> [Int]{
     let tt = numbers.sorted()
+    let counts = tt.count
     
     return tt
 }
@@ -10,4 +11,5 @@ print(solution([1,5,3]))
 
 let level1 = level_1()
 level1.solution(10)
+level1.solution("pYy")
 
--- Programmers/programmers_1.playground/Sources/Level_1.swift
+++ Programmers/programmers_1.playground/Sources/Level_1.swift
@@ -16,7 +16,24 @@ public class level_1{
         }
         return count
     }
-    
+    //문자열 내 p와 y의 개수
+    public func solution(_ s: String) -> Bool{
+        // components, filter.count 가능
+        var Pcounts = 0
+        var Ycounts = 0
+        let ans: Bool = false
+        for i in s{
+            if i == "P" || i == "p"{
+                Pcounts = Pcounts + 1
+            }else if i == "Y" || i == "y"{
+                Ycounts = Ycounts + 1
+            }
+        }
+        if Pcounts == Ycounts{
+            return true
+        }
+        return ans
+    }
     
 }
 
