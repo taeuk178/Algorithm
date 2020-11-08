0b52c6838c2aa5d364e42247a52dc50c7c2b609b
taeuk <nous178@naver.com>
2020-11-08 22:12:23 +0900

    commit

--- Programmers/programmers_1.playground/Contents.swift
+++ Programmers/programmers_1.playground/Contents.swift
@@ -22,7 +22,31 @@ func solution(_ numbers: [Int]) -> [Int]{
 }
 
 //print(solution([2,1,3,4,1]))
-
+func solution11(_ s: String, _ n: Int) -> String{
+   var answer = String()
+    let alphabet = ["A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":15,"Q":16,"R":17,"S":18,"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25]
+    let keys = alphabet.keys.sorted()
+    
+    for i in 0..<Array(s).count{
+        if Array(s)[i] == " "{
+            answer += " "
+            continue
+        }
+        var number = alphabet[String(Array(s)[i].uppercased())]! + n
+        if number > 25 {
+            number = number - 26
+        }
+        let value = keys[number]
+        let value1 = alphabet.index(forKey: value)
+        if Array(s)[i].isUppercase{
+            answer += alphabet.keys[value1!]
+        }else {
+            answer += alphabet.keys[value1!].lowercased()
+        }
+    }
+    return answer
+}
+print(solution11("ab", 1))
 let level1 = level_1()
 
 level1.solution1(10)
@@ -35,3 +59,4 @@ level1.solution7(5)
 level1.solution8(2, 5)
 level1.solution9("Zbcdefg")
 level1.solution10([2,5,7,10], 5)
+level1.solution11("ab", 1)
--- Programmers/programmers_1.playground/Sources/Level_1.swift
+++ Programmers/programmers_1.playground/Sources/Level_1.swift
@@ -129,5 +129,29 @@ public class level_1{
             return [-1]
         }
     }
+    public func solution11(_ s: String, _ n: Int) -> String{
+        var answer = String()
+        let alphabet = ["A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":15,"Q":16,"R":17,"S":18,"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25]
+        let keys = alphabet.keys.sorted()
+        
+        for i in 0..<Array(s).count{
+            if Array(s)[i] == " "{
+                answer += " "
+                continue
+            }
+            var number = alphabet[String(Array(s)[i].uppercased())]! + n
+            if number > 25 {
+                number = number - 26
+            }
+            let value = keys[number]
+            let value1 = alphabet.index(forKey: value)
+            if Array(s)[i].isUppercase{
+                answer += alphabet.keys[value1!]
+            }else {
+                answer += alphabet.keys[value1!].lowercased()
+            }
+        }
+        return answer
+    }
 }
 
