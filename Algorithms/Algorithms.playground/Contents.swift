f513f6269abb78e48f78c79d3e9e2d0a295d3623
taeuk <nous178@naver.com>
2021-02-03 21:53:06 +0900

    commit

--- Algorithms/Algorithms.playground/Contents.swift
+++ Algorithms/Algorithms.playground/Contents.swift
@@ -1,25 +1,57 @@
 import UIKit
+//
+//let grid = Grid()
+//
+//func physical(_ n: Int, _ lost: [Int], _ reserve: [Int]) -> Int{
+//
+//    var count = 0
+//
+//    for i in 1...n{
+//        var tt = lost.filter{$0 == i}
+//        print(tt)
+//
+//
+//    }
+//    return 0
+//}
+//print(physical(5, [2,4], [1,3,5]))
+//
+//let sort = Sorts()
+//var arrays = [1,5,6,4,2]
+////sort.selectionSort()
+////sort.bubbleSort()
+////sort.insertionSort(array: arrays)
+////sort.heapSort(&arrays)
+//sort.split(data: arrays)
+//func gridcoin(){
+//    var n = 1260
+//    var count = 0
+//
+//    let coinTypes = [500, 100, 50, 10]
+//
+//    for coin in coinTypes {
+//        count += n / coin
+//        print("count , \(n/coin)")
+//        n %= coin
+//        print("n: \(n)")
+//    }
+//
+//    print(count)
+//}
 
-let grid = Grid()
-
-func physical(_ n: Int, _ lost: [Int], _ reserve: [Int]) -> Int{
+func atm11399() -> Int{
     
-    var count = 0
+    let n = Int(readLine()!)!
+    var arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
     
-    for i in 1...n{
-        var tt = lost.filter{$0 == i}
-        print(tt)
-
-        
+    var result = 0
+    var tmp = 0
+    
+    for num in arr {
+        tmp += num
+        result += tmp
     }
-    return 0
+    return result
 }
-print(physical(5, [2,4], [1,3,5]))
+print(atm11399())
 
-let sort = Sorts()
-var arrays = [1,5,6,4,2]
-//sort.selectionSort()
-//sort.bubbleSort()
-//sort.insertionSort(array: arrays)
-//sort.heapSort(&arrays)
-sort.split(data: arrays)
--- Algorithms/Algorithms.playground/Sources/Grid.swift
+++ Algorithms/Algorithms.playground/Sources/Grid.swift
@@ -18,4 +18,42 @@ public class Grid{
 
         print(count)
     }
+    //baekjoon
+    public func grid11047() -> Int{
+        let temp = readLine()!.split(separator: " ").map({Int($0)!})
+        let n = temp[0]
+        var k = temp[1]
+        
+        var coins = [Int]()
+        var result: Int = 0
+        for _ in 0..<n{
+            coins.append(Int(readLine()!)!)
+        }
+        coins.reverse()
+        for value in coins{
+            if value <= k{
+                result += (k / value)
+                k = (k % value)
+            }
+            if k == 0{
+                break
+            }
+        }
+        return result
+    }
+    public func atm11399() -> Int{
+        
+        let n = Int(readLine()!)!
+        var arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+        
+        var result = 0
+        var tmp = 0
+        
+        for num in arr {
+            tmp += num
+            result += tmp
+        }
+        return result
+    }
+
 }
