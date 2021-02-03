c64ab240efef79f5f2a9c05f58854158e329ec19
taeuk <nous178@naver.com>
2021-02-03 19:19:02 +0900

    commit

--- Baek/BaekJoon/BaekJoon/main.swift
+++ Baek/BaekJoon/BaekJoon/main.swift
@@ -7,8 +7,19 @@
 
 import Foundation
 
-var input = readLine() ?? ""
+
+let input = readLine() ?? ""
 let a = Int(input) ?? 0
+
+let inputtwo = readLine() ?? ""
+let b = Int(inputtwo) ?? 0
+
+print(a*(b%10))
+print(a*(b%100-b%10)/10)
+print(a*(b/100))
+print(a*b)
+
+
 
 // star_1
 //for i in 1...a{
@@ -39,14 +50,28 @@ let a = Int(input) ?? 0
 //}
 
 // star_4
-for i in 1...a{
-    var result = ""
-    for _ in a-i+1..<a{
-        result += " "
-    }
-    for _ in 1...a-i+1{
-        result += "*"
-    }
-    print(result)
-}
+//for i in 1...a{
+//    var result = ""
+//    for _ in a-i+1..<a{
+//        result += " "
+//    }
+//    for _ in 1...a-i+1{
+//        result += "*"
+//    }
+//    print(result)
+//}
 
+// 배열 2577
+
+//var N = 1
+//var dic = [Int](repeating: 0, count: 10)
+//(0..<3).forEach { _ in
+//    N *= Int(readLine()!)!
+//}
+//
+//while N > 0{
+//    let rem = N % 10
+//    dic[rem] += 1
+//    N /= 10
+//}
+//dic.forEach { print($0) }
--- C:Swift/Swift_Ver.playground/Contents.swift
+++ C:Swift/Swift_Ver.playground/Contents.swift
@@ -36,6 +36,53 @@ func q15(_ height: Int, _ width: Int) -> String{
     }
     return ""
 }
-print(q15(3, 7))
+//print(q15(3, 7))
 
+//최솟값 구하기
+func q1(_ a: [Int]) -> Int{
+    //a[0] 값 하나받음
+    var max = a[0]
+    //a를 a[1] 부터 a.count 까지 돌림
+    for i in a[1..<a.count]{
+        // i 가 a[0]보다 크면 바꿈
+        if i < max{
+            max = i
+        }
+    }
+    return max
+}
+//print(q1([3,1,4,7]))
 
+//배열의 합계
+func q2(_ a: [Int]) -> Int{
+    var count = 0
+    
+    for i in a[0..<a.count]{
+        count += i
+    }
+    return count
+}
+//print(q2([5,3,2,1]))
+
+//평균값
+func q3(_ a: [Int]) -> Int{
+    var count = 0
+    
+    for i in a[0..<a.count]{
+        count += i
+        
+    }
+    count = count / a.count
+    return count
+}
+
+// 배열 복사, 역순
+func q8(_ b: [Int]) -> [Int]{
+    var a: [Int] = []
+    for i in b[0..<b.count]{
+        a[0] = b[0]
+    }
+//    a.reverse()
+    return a
+}
+print(q8([1,2,3,4,7]))
