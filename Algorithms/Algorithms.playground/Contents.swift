8ae133a1e92125c7cc5e4dda7827e541f13758ba
taeuk <nous178@naver.com>
2021-02-04 15:21:17 +0900

    grid

--- Algorithms/Algorithms.playground/Contents.swift
+++ Algorithms/Algorithms.playground/Contents.swift
@@ -39,19 +39,5 @@ import UIKit
 //    print(count)
 //}
 
-func atm11399() -> Int{
-    
-    let n = Int(readLine()!)!
-    var arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
-    
-    var result = 0
-    var tmp = 0
-    
-    for num in arr {
-        tmp += num
-        result += tmp
-    }
-    return result
-}
-print(atm11399())
+
 
--- Algorithms/Algorithms.playground/Sources/Grid.swift
+++ Algorithms/Algorithms.playground/Sources/Grid.swift
@@ -56,4 +56,23 @@ public class Grid{
         return result
     }
 
+    public func gird2839() -> Int{
+        var n = Int(readLine()!)!
+        var result = 0
+        
+        if n % 5 == 0{
+            return n / 5
+        }
+        
+        while (n >= 0){
+            n -= 3
+            result += 1
+            if n % 5 == 0{
+                return result + (n / 5)
+            }
+        }
+        
+        
+        return -1
+    }
 }
