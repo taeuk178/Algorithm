6de342c5449c2712ee827982033e4669a2f7c50d
taeuk <nous178@naver.com>
2021-02-08 21:39:43 +0900

    BOJ_Stack10828

--- Baek/BaekJoon/BaekJoon/main.swift
+++ Baek/BaekJoon/BaekJoon/main.swift
@@ -7,45 +7,45 @@
 
 import Foundation
 
-func solution() -> Int{
+let input = Int(readLine() ?? "") ?? 0
+var stack: [Int] = []
+func empty() -> Int {
+    return stack.isEmpty ? 1 : 0
+    
+}
+func top() -> Int {
+    return stack.first ?? -1
+}
+func size() -> Int {
+    return stack.count
+    
+}
+func pop() -> Int {
+    return stack.isEmpty ? -1 : stack.removeFirst()
+    
+}
+func push(_ input: Int) {
+    stack.insert(input, at: 0)
+    
+}
+for _ in 0..<input {
     let input = readLine() ?? ""
-    let n = Int(input) ?? 0
-
-    var ropes = [Int]()
-    for _ in 0..<n{
-        ropes.append(Int(readLine()!)!)
+    switch input {
+    case "pop":
+        print(pop())
+    case "size":
+        print(size())
+    case "empty":
+        print(empty())
+    case "top": print(top())
+    case let x where x.contains("push"):
+        let input = Int(x.replacingOccurrences(of: "push ", with: "")) ?? 0
+        push(input)
+    default:
+        break
     }
-    ropes = ropes.sorted()
-        let strongestRope = ropes[n-1]
-        var result = 0
-        
-        for (index,weakestRope) in ropes.enumerated(){
-            if (weakestRope * (n-index)) > result {
-                result = (weakestRope * (n - index))
-            }
-        }
-        
-        if result > strongestRope{
-            return result
-        }
+}
 
-        return strongestRope
-}
-//print(solution())
-func grid14916() -> Int{
-    var n = Int(readLine()!)!
-    var count = 0
-    var o = n % 5
-    if n == 1 || n == 3{
-        count = -1
-    }else if o % 2 == 0{
-        count = n / 5 + o / 2
-    }else{
-        count = ((n / 5)-1) + ((o + 5)/2)
-    }
-    return count
-}
-print(grid14916())
 
 //lopes = lopes.sorted()
 //var tt = lopes.map{$0 * a}
